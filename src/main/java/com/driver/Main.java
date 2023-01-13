65e3532d344fd617a8c7a2f9a170bfd455061440
ShubhKrishna15 <Krsnavasudey@gmail.com>
2023-01-13 22:33:50 +0530

    Poly-solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,31 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(5,5);
+        p.product(5,5,5);
+        p.product(5.5,5.0);
+
+    }
+    public static class Product {
+
+        public int product(int x, int y) {
+            return x*y ;
+        }
+
+        public int product(int x, int y, int z) {
+
+            return x*y*z ;
+
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+
+
+    }
+
 }
